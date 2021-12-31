b2e5a1b2a203f30a11070b8c77e36b809932bf59
fanyongpeng <fanyongpeng@126.com>
2021-12-31 11:35:42 +0800

    add

--- src/main/java/io/jingwei/registry/LoadData.java
+++ src/main/java/io/jingwei/registry/LoadData.java
@@ -218,8 +218,8 @@ public class LoadData {
                 res[0]=0;
                 System.arraycopy(pubkey, 0, res, 1, pubkey.length);
                 System.arraycopy(checksum, 0, res, pubkey.length+1, 4);
-//                System.out.println(Base58Util.encode(res));
-                System.out.println(peer);
+                System.out.println(Base58Util.encode(res));
+//                System.out.println(peer);
             } catch (Exception e) {
 //                e.printStackTrace();
             }
